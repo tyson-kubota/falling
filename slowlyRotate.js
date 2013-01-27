0609a759deb5fcb5678a0579c7154a626d5d8b7a
tyson-kubota <tyson.kubota.web@gmail.com>
2013-01-27 11:40:27 -0500

    object rotation only when visible

--- slowlyRotate.js
+++ slowlyRotate.js
@@ -1,10 +1,14 @@
 #pragma strict
 
 var dir:float = 1;
+var shouldRotate : boolean = false;
 
-function Start () {
-}
+function Start () {}
 
 function Update () {
-    transform.Rotate(Vector3.up * (dir*3) * Time.deltaTime, Space.World);
+	if (shouldRotate == true) {transform.Rotate(Vector3.up * (dir*3) * Time.deltaTime, Space.World);}
 }
+
+// toggles rotation based on object visibility
+function OnBecameVisible() {shouldRotate = true;}
+function OnBecameInvisible() {shouldRotate = false;}
