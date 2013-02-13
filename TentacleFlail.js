3c03871fc42406d3a2d0f689387fe5942601f9a9
tyson-kubota <tyson.kubota.web@gmail.com>
2013-02-13 01:02:46 -0500

    Tentacles should swat you dynamically as you approach
    by crossfading animations when you trip sphere collider triggers on the
    tentacle meshes

---
+++ TentacleFlail.js
@@ -0,0 +1,17 @@
+#pragma strict
+
+var mainAnimation : String;
+var triggerAnimation : String;
+
+function OnTriggerEnter (other : Collider) {
+  if (other.gameObject.CompareTag ("Player")) {
+	animation.CrossFade(triggerAnimation, 0.5);
+  }	
+}
+
+function OnTriggerExit (other : Collider) {
+  if (other.gameObject.CompareTag ("Player")) {
+	//animation.PlayQueued("twist2", QueueMode.PlayNow);
+  	animation.CrossFade(mainAnimation, 1.0);
+  }	
+}
