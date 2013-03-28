1e84a3d331360f8d16430b334ddc0842355c9ccd
tyson-kubota <tyson.kubota.web@gmail.com>
2013-03-27 23:39:34 -0400

    crude UV texture offset animation, like for flowing lava

---
+++ AnimatedFlowUVs.js
@@ -0,0 +1,17 @@
+#pragma strict
+
+var speed:float = 1;
+var shouldAnimate : boolean = false;
+
+function Start () {}
+
+function Update () {
+	if (shouldAnimate == true) {
+		var offset = Time.time * speed;
+		renderer.sharedMaterial.mainTextureOffset = Vector2(0,offset%1);
+	}
+}
+
+// toggles rotation based on object visibility
+function OnBecameVisible() {shouldAnimate = true;}
+function OnBecameInvisible() {shouldAnimate = false;}
