30f0e569cfa9bb9a3db3ea754d032051560404fe
tyson-kubota <tyson.kubota.web@gmail.com>
2013-04-10 14:48:35 -0400

    option to grow projectiles at launch, so they don't pop in at full scale

--- ProjectileDestroy.js
+++ ProjectileDestroy.js
@@ -1,9 +1,12 @@
 #pragma strict
 
 var removeTime : float = 10.0;
+var growAtLaunch : boolean = false;
+var growTime : float = 7.0;
 
 function Start () {
 	Destroy(gameObject, removeTime);
+	if (growAtLaunch == true) {growProjectile (growTime);}
 }
 
 // this is so fireballs that hit the player don't interfere with newly-spawned ones in the scene.
@@ -14,7 +17,23 @@ function OnCollisionEnter (collision : Collision) {
   }
   
 // but if one fireball hits another, destroy immediately.
-  if (collision.gameObject.CompareTag ("Death")) {
+  if (this.gameObject.layer == 17 && collision.gameObject.CompareTag ("Death")) {
 	Destroy(gameObject, 0);
   }
+}
+
+function growProjectile (timer : float) {
+
+//	transform.localScale += Vector3(0,0,0);
+
+    var start = Vector3(0,0,0);
+    var end = transform.localScale;
+    var i = 0.0;
+    var step = 1.0/timer;
+
+    while (i <= 1.0) {
+        i += step * Time.deltaTime;
+        transform.localScale = Vector3.Lerp(start, end, i);
+        yield;
+    }
 }
