52428e89b6d2ead3ff2e6a2b29e886a969a708e4
tyson-kubota <tyson.kubota.web@gmail.com>
2013-06-11 10:10:16 -0400

    first attempt at iPad device detection
    could be cleaned up with fewer OR statements

--- FallingLaunch.js
+++ FallingLaunch.js
@@ -3,6 +3,17 @@ static var flipMultiplier : int = -1;
 static var landscapeFlipped:boolean = false;
 static var levelEndSlowdown:int = 0;
 var targetFPS : int = 30;
+static var isTablet : boolean = false;
+static var iOSGen = iPhone.generation;
+
+enum iPads {
+	iPadUnknown,
+	iPad1Gen,
+	iPad2Gen,
+	iPad3Gen,
+	iPad4Gen,
+	iPadMini1Gen
+};
 
 function Awake () {
 	Application.targetFrameRate = targetFPS;
@@ -15,6 +26,15 @@ function Awake () {
 	else {	Screen.orientation = ScreenOrientation.LandscapeLeft;
 		flipMultiplier = 1;
 	}
+	
+	iOSGen = iPhone.generation;
+	
+	if ((iOSGen && 
+	(iPads.iPad1Gen | iPads.iPad2Gen | iPads.iPad3Gen | iPads.iPad4Gen | iPads.iPadMini1Gen | iPads.iPadUnknown)) != 0) {
+		isTablet = true;
+		flipMultiplier = 2 * flipMultiplier;
+		//Debug.Log("this is an " + iOSGen  + " iPad!");
+	};
 	
 	DontDestroyOnLoad (this);
 //	Application.LoadLevel("Falling-scene-menu");
