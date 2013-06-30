795fe0385724803ce0b9a067c313174b2517693d
tyson-kubota <tyson.kubota.web@gmail.com>
2013-06-30 10:33:34 -0400

    uv animation fix to keep offset below 1

--- AnimatedFlowUVs.js
+++ AnimatedFlowUVs.js
@@ -2,16 +2,29 @@
 
 var speed:float = 1;
 var shouldAnimate : boolean = false;
+private var offset : float = 0;
+private var offsetStartTime : float;
+private var offsetTimer : float = 0;
 
-function Start () {}
+function Start () {
+	renderer.sharedMaterial.mainTextureOffset = Vector2(0,0);
+}
 
 function Update () {
 	if (shouldAnimate == true) {
-		var offset = Time.time * speed;
+		offset = offsetTimer * speed;
+		
+		if ((offsetTimer*speed) >= 1) {
+			offsetStartTime = Time.time;
+		}
+		
+		offsetTimer = (Time.time - offsetStartTime);
+	
+		Debug.Log(offsetTimer);
 		renderer.sharedMaterial.mainTextureOffset = Vector2(0,offset);
 	}
 }
 
-// toggles rotation based on object visibility
-function OnBecameVisible() {shouldAnimate = true;}
+// toggles offset changes based on object visibility
+function OnBecameVisible() {shouldAnimate = true; offsetStartTime = Time.time;}
 function OnBecameInvisible() {shouldAnimate = false;}
